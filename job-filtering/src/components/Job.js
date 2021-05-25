877f8504198f602cbfaba2c0462699e143f0cfb2
bur4k58 <s116768@ap.be>
2021-05-25 15:16:57 +0200

    Mobile responsive

--- job-filtering/src/components/Job.js
+++ job-filtering/src/components/Job.js
@@ -1,27 +1,27 @@
 import React, { useState} from "react";
 
 export default function Job({ job }) {
-  const [languages, setLanguages] = useState([...job.languages, ...job.tools, job.role, job.level]);
+  const [languages, setLanguages] = useState([job.role, job.level, ...job.languages, ...job.tools]);
 
   return (
-    <div className={`flex shadow-lg mx-20 m-5 mx-5 p-5 bg-white ${job.featured && 'border-l-4 border-primary'}`}>
+    <div className={`flex flex-col shadow-md mx-5 m-5 p-5 my-16 bg-white ${job.featured && 'border-l-4 border-primary rounded'} mobile:flex-row mobile:mx-20`}>
       <div>
-        <img src={`${job.logo}`} alt={job.company} />
+        <img src={`${job.logo}`} alt={job.company} className="-mt-16 mb-4 w-16 h-16 mobile:mt-0 mobile:w-24 mobile:h-24"/>
       </div>
-      <div className="flex flex-col ml-5 justify-between">
-        <div className="flex space-x-3">
+      <div className="flex flex-col justify-between px-2 mobile:ml-5">
+        <div className="flex space-x-3  flex-wrap">
           <h3 className="text-primary font-bold">{job.company}</h3>
           {job.new ? <span className="rounded-full px-2 p-1 bg-primary text-white font-bold">NEW!</span> : null}
-          {job.featured ? <span className="rounded-full px-2 p-1 bg-verydark text-white font-bold">FEAUTRED</span> : null}
+          {job.featured ? <span className="rounded-full px-2 py-1 bg-verydark text-white font-bold">FEAUTRED</span> : null}
         </div>
         <h2 className="font-bold">{job.position}</h2>
-        <p className="text-gray-400">
-          {job.postedAt} * {job.contract} * {job.location}
+        <p className="text-dark">
+          {job.postedAt} · {job.contract} · {job.location}
         </p>
       </div>
-      <div className="flex items-center justify-center ml-auto">
+      <div className="flex items-center flex-wrap border-t-2 pt-4 mt-4 mobile:ml-auto mobile:border-0 mobile:pt-0 mobile:mt-0">
         {languages.length > 0
-          ? languages.map((x) => <p className="text-primary bg-background font-bold m-2 p-2 rounded" key={x}>{x}</p>)
+          ? languages.map((x) => <p className="flex text-primary bg-background font-bold m-2 p-2 rounded" key={x}>{x}</p>)
           : null
         }
       </div>
